71b998f10f56ad91fbda42b0e944656af55eacf7
HenriqueHeiden <henrique.heiden@gmail.com>
2020-02-11 18:50:24 -0300

    Ajustes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { MdShoppingBasket } from "react-icons/md";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { Container, Cart, img } from "./styles";
 
 import logo from "../../assets/images/logo.png";
 
-function Header({ cartSize }) {
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -25,6 +27,3 @@ function Header({ cartSize }) {
     </Container>
   );
 }
-export default connect(state => ({
-  cartSize: state.cart.length
-}))(Header);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,68 +1,57 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { MdAddShoppingCart } from "react-icons/md";
 import api from "../../services/api";
-import { bindActionCreators } from "redux";
 import * as CartActions from "../../store/module/cart/actions";
 
 import { ProductList } from "./styles";
 import { formatPrice } from "../../util/format";
 
-class Home extends Component {
-  state = {
-    products: []
-  };
-
-  async componentDidMount() {
-    const response = await api.get("products");
-    const data = response.data.map(product => ({
-      ...product,
-      priceFormatted: formatPrice(product.price)
-    }));
-    this.setState({ products: data });
-  }
-
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
-
-    addToCartRequest(id);
-  };
-
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
-    return (
-      <ProductList>
-        {products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormatted}</span>
-            <button
-              type="button"
-              onClick={() => this.handleAddProduct(product.id)}
-            >
-              <div>
-                <MdAddShoppingCart size={16} color="#FFF" />{" "}
-                {amount[product.id] || 0}
-              </div>
-              <span>Add no carrinho</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
-    return amount;
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const amount = useSelector(state => state.cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount;
+    return sumAmount;
   }, {})
-});
+  );
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get("products");
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price)
+      }));
+      setProducts(data);
+    }
+    loadProducts();
+  }, []);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+  function handleAddProduct(id) {
+    dispatch(CartActions.addToCartRequest(id));
+  };
+
+
+  return (
+    <ProductList>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />{" "}
+              {amount[product.id] || 0}
+            </div>
+            <span>Add no carrinho</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
+}
